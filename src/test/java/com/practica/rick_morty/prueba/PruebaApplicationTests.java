5f15f863d040f41beaa0ae6f9c790f7b6c7fc532
Dani Z <manchess@hotmail.es>
2023-08-15 02:42:06 -0300

    Proyecto en blanco

---
+++ src/test/java/com/practica/rick_morty/prueba/PruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.rick_morty.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
