3813596c652e095014c18a70f0dfc5ba37fffdd4
Dani Z <manchess@hotmail.es>
2023-08-15 23:59:04 -0300

    Se agrego una nueva dep para validaciones , config de bd

---
+++ src/main/java/com/practica/rick_morty/prueba/controllers/CharacterController.java
@@ -0,0 +1,20 @@
+package com.practica.rick_morty.prueba.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/rick-and-morty/")
+public class CharacterController {
+
+    @GetMapping("RMApiCharacters")
+    public void getApiCharacters() {
+
+    }
+
+    @GetMapping("/characters")
+    public void getCharacters() {
+
+    }
+}
